7438ec6a8916bfff8e2241633ef383466269f711
Aryaman Verma <amazingayv@gmail.com>
2025-12-23 14:55:29 +0530

    Add first-time, mood and history routes

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/first-time")
+def first_time():
+    return render_template("first_time.html")
+
+@app.route("/mood")
+def mood():
+    return render_template("mood.html")
+
+@app.route("/history")
+def history():
+    return render_template("history.html")
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
