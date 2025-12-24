25a3596368cb4b67779f048a18ccde2a75f91d2c
Aryaman Verma <amazingayv@gmail.com>
2025-12-24 12:15:48 +0530

    Disable custom player while using Spotify iframe

--- static/js/player.js
+++ static/js/player.js
@@ -1,15 +1,3 @@
-const audio = document.getElementById("audio");
-const playBtn = document.getElementById("play-btn");
-
-async function loadAndPlay() {
-  const res = await fetch("/api/songs");
-  const songs = await res.json();
-  if (!songs.length) return;
-  if (!songs[0].preview_url) return;
-  audio.src = songs[0].preview_url;
-  audio.play();
-}
-
-if (playBtn) {
-  playBtn.addEventListener("click", loadAndPlay);
-}
+document.addEventListener("DOMContentLoaded", () => {
+  return;
+});
